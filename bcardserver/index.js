8f7ad9de94d3ed1bccfa648336155f584c9a83f4
ofeknave99 <ofeknave99@gmail.com>
2023-09-21 11:41:30 +0300

    chages index

--- bcardserver/index.js
+++ bcardserver/index.js
@@ -18,7 +18,7 @@ mongoose
   .catch((err) => console.log(err));
 
     app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(chalk.bgWhite(`Server is running on port ${PORT}`));
 });
     app.use(express.json());
     app.use(cors());
